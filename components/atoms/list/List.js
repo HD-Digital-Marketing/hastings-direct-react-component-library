057fc632d4d6624f00291c69fe300c164f63727c
darrenwhatford <darrenwhatford@hastingsdirect.com>
2020-12-16 14:05:45 +0000

    DPW added content to the List component, taken from the Button component as a template

--- components/atoms/list/List.js
+++ components/atoms/list/List.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import classNames from 'classnames';
 import './list.scss';
+
+const List = (props) => {
+
+    const {
+        children,
+        type = 'list',
+        theme = ''
+    } = props;
+
+    let listClass = classNames({
+        'a-list': true,
+        'a-list--primary': theme === 'primary',
+        'a-list--secondary': theme === 'secondary',
+
+    });
+
+    return(
+        <ul className={listClass} type={type}>{children}</ul>
+    )
+
+}
 
 export default List;

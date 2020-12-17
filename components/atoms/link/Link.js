7190df68fe1fac5cbf957be1a454051bc1b01acb
hdfeddavidevans <davidevans@hastingsdirect.com>
2020-12-17 11:50:10 +0000

    DE add props & class names

--- components/atoms/link/Link.js
+++ components/atoms/link/Link.js
@@ -2,4 +2,24 @@ import React from 'react';
 import classNames from 'classnames';
 import './link.scss';
 
+const Link = (props) => {
+
+	const {
+		children,
+		type = 'lnk'
+	} = props;
+
+	let linkClass = classNames({
+		'a-link-image': true,
+		'a-link-image__image': true,
+		'a-link-image__title': true,
+		'external': true
+	});
+
+	return(
+		<a className={linkClass}>{children}</a>
+	)
+
+}
+
 export default Link;

c06a4be7c2a77b5508bdcdc772685bbf183736d0
Dan Jackson <danjackson@hastingsdirect.com>
2020-12-21 14:36:46 +0000

    DJ PropTypes check added to the link component

--- components/atoms/link/Link.js
+++ components/atoms/link/Link.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import classNames from 'classnames'
+import PropTypes from 'prop-types'
 import './link.scss'
 
 const Link = props => {
 
 	const {
-		children,
 		href = '',
 		rel = '',
 		role = '',
 		target = '',
-		theme = ''
+		theme = '',
+		children
 	} = props
 
 	const linkClass = classNames('a-link', className, {
@@ -21,6 +22,15 @@ const Link = props => {
 	return(
 		<a {...{ href, rel, role, target }} className={linkClass}>{children}</a>
 	)
+}
+
+Link.propTypes = {
+	href: PropTypes.string,
+	rel: PropTypes.string,
+	role: PropTypes.string,
+	target: PropTypes.string,
+	theme: PropTypes.string,
+	children: PropTypes.string
 }
 
 export default Link

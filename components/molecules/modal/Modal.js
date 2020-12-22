a6f3625fd9c859c5f45d93c06131b51e937ef5c2
Dan Jackson <danjackson@hastingsdirect.com>
2020-12-22 11:26:24 +0000

    DJ Additional structure added to the modal component to add styling

--- components/molecules/modal/Modal.js
+++ components/molecules/modal/Modal.js
@@ -4,9 +4,19 @@ import './modal.scss'
 
 const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
 	<>
-		<div className="modal">
-			<h1>Hello Modal</h1>
-		</div>
+    	<div className="modal-overlay"/>
+    		<div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+      			<div className="modal">
+        			<div className="modal-header">
+          				<button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+            				<span aria-hidden="true">&times;</span>
+         				 </button>
+        			</div>
+        		<p>
+          			Hello, I'm a modal.
+        		</p>
+      		</div>
+    	</div>
 	</>, document.body
 ) : null
 

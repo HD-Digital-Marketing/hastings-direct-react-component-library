6319478d0745b8b2a13c26879277aa1415d3492b
Dan Jackson <danjackson@hastingsdirect.com>
2020-12-22 11:57:38 +0000

    DJ Small structure changes

--- components/molecules/modal/Modal.js
+++ components/molecules/modal/Modal.js
@@ -5,13 +5,13 @@ import './modal.scss'
 const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
 	<>
     	<div className="modal-overlay"/>
-    		<div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
-      			<div className="modal">
-        			<div className="modal-header">
-          				<button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
-            				<span aria-hidden="true">&times;</span>
-         				 </button>
-        			</div>
+    	<div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+      		<div className="modal">
+        		<div className="modal-header">
+          			<button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+            			<span aria-hidden="true">&times;</span>
+         			</button>
+        		</div>
         		<p>
           			Hello, I'm a modal.
         		</p>

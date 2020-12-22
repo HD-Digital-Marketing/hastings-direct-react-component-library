d63d027e96fc0642fea390d9b0b45721a43fa45a
Dan Jackson <danjackson@hastingsdirect.com>
2020-12-22 10:23:51 +0000

    DJ Initial code for the modal component added

---
+++ components/molecules/modal/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
+	<>
+		<div className="modal">
+			<h1>Hello Modal</h1>
+		</div>
+	</>, document.body
+) : null
+
+export default Modal;

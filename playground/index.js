7627c90516719893954b09d6991d679d3088eb16
Dan Jackson <danjackson@hastingsdirect.com>
2020-12-22 11:03:54 +0000

    DJ Add initial modal code to the playground for testing inside the component library

--- playground/index.js
+++ playground/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Title from '../components/atoms/title/Title';
-import Button from '../components/atoms/button/Button';
+import Modal from '../components/molecules/modal/Modal';
+import useModal from '../components/molecules/modal/useModal';
 
 const Playground = () => {
+
+	const {isShowing, toggle} = useModal();
+
     return (
         <>
-            <Title titleType="h2" theme="secondary">A h2 title</Title>
-            <Button type="submit" theme="primary">Click me</Button>
+            <div className="App">
+            	<button className="button-default" onClick={toggle}>Show Modal</button>
+            	<Modal isShowing={isShowing} hide={toggle} />
+            </div>
         </>
     )
 }

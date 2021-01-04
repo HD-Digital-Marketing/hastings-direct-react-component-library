44a7b4e2a4a52d45b00724efb0e90af876e938d4
Scott Quested <ScottQuested@hastingsdirect.com>
2021-01-04 15:07:29 +0000

    SQ Update input with extra attributes. Fix test to check for these attributes.

--- components/atoms/input/Input.js
+++ components/atoms/input/Input.js
@@ -7,6 +7,19 @@ const Input = props => {
 
     const {
         type = 'text',
+        id = '',
+        size = '',
+        length = '',
+        maxLength = '',
+        readOnly = false,
+        disabled = false,
+        min = '',
+        max = '',
+        multiple = false,
+        required = false,
+        step = '',
+        autoFocus = false,
+        autoComplete = 'on',
         name = '',
         placeholder = '',
         value = '',
@@ -16,19 +29,44 @@ const Input = props => {
     const inputClass = classNames('a-input', className)
 
     return (
-        <input
-            type={type}
-            name={name}
-            className={inputClass}
-            placeholder={placeholder}
-            defaultValue={value}
-        />
+        <input {...{
+            id,
+            size,
+            length,
+            maxLength,
+            readOnly,
+            disabled,
+            min,
+            max,
+            multiple,
+            required,
+            step,
+            autoFocus,
+            autoComplete,
+            type,
+            name,
+            placeholder }}
+               className={inputClass}
+               defaultValue={value} />
     )
 }
 
 Input.propTypes = {
     type: PropTypes.string,
     name: PropTypes.string,
+    id: PropTypes.string,
+    size: PropTypes.string,
+    length: PropTypes.string,
+    maxLength: PropTypes.string,
+    readOnly: PropTypes.bool,
+    disabled: PropTypes.bool,
+    min: PropTypes.string,
+    max: PropTypes.string,
+    multiple: PropTypes.bool,
+    required: PropTypes.bool,
+    step: PropTypes.string,
+    autoFocus: PropTypes.bool,
+    autoComplete: PropTypes.string,
     placeholder: PropTypes.string,
     value: PropTypes.string,
     className: PropTypes.string,
--- components/atoms/input/Input.test.js
+++ components/atoms/input/Input.test.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import Input from './Input';
+import React from 'react'
+import Input from './Input'
 
 describe('Check the input renders', () => {
     test('it should render the input', () => {
 
         const renderedInput = Input({
             type: 'search',
+            id: 'someID',
             value: '1234',
             name: 'testInput',
             placeholder: 'Test text'
         });
 
-        expect(renderedInput).toEqual(<input type="search" name="testInput" className="a-input" placeholder="Test text" defaultValue="1234" />);
+        expect(renderedInput).toEqual(
+            <input id="someID"
+                   length=""
+                   maxLength=""
+                   size=""
+                   min=""
+                   max=""
+                   step=""
+                   autoComplete="on"
+                   type="search"
+                   name="testInput"
+                   placeholder="Test text"
+                   className="a-input"
+                   defaultValue="1234"
+                   autoFocus={false}
+                   disabled={false}
+                   multiple={false}
+                   readOnly={false}
+                   required={false}
+            />
+            )
     });
 });

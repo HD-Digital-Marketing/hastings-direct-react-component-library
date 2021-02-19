9841561dc78438766ba6bb367d8f2b7095ff6134
Dan Jackson <danjackson@hastingsdirect.com>
2021-02-19 13:44:04 +0000

    DJ Initial code added to build the message component

--- components/atoms/message/Message.js
+++ components/atoms/message/Message.js
@@ -2,3 +2,29 @@ import React from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import './message.scss'
+
+const Message = props => {
+
+	const {
+		theme = '',
+		className = '',
+		children
+	} = props
+
+	const messageClass = classNames('a-message', className, {
+		'a-message--primary': theme === 'primary',
+		'a-message--primary-on-dark': theme === 'primary-dark'
+	})
+
+	return(
+		<div>Hello</div>
+	)
+}
+
+Message.propTypes = {
+	theme: PropTypes.string,
+	className: PropTypes.string,
+	children: PropTypes.string
+}
+
+export default Message
--- playground/index.js
+++ playground/index.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Header from '../components/atoms/header/Header';
+import Message from '../components/atoms/message/Message';
 
 const Playground = () => {
 
     return (
         <>
-
             <div className="playground-container">
-                <div className="playground-container__item">
-                    <Header headerType="h1" theme="primary-dark-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h1" theme="secondary-dark-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h1" theme="primary-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h1" theme="secondary-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h2" theme="primary-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h2" theme="secondary-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h3" theme="primary-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h3" theme="secondary-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h4" theme="primary-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h4" theme="secondary-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                </div>
-                <div className="playground-container__item">
-                    <Header headerType="h1" theme="primary-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h1" theme="secondary-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h1" theme="primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h1" theme="secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h2" theme="primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h2" theme="secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h3" theme="primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h3" theme="secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h4" theme="primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                    <Header headerType="h4" theme="secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Header>
-                </div>
+                <Message />
             </div>
         </>
     )

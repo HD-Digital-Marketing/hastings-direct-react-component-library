00deab21e3df0cff1ab6dc43c56ef3e044e30139
Scott Quested <ScottQuested@hastingsdirect.com>
2021-03-16 11:33:39 +0000

    SQ Add the styles for panel component and add the SVG for the patterned background.

--- playground/index.js
+++ playground/index.js
@@ -3,21 +3,15 @@ import {render} from 'react-dom'
 import Container from '../components/utility/layout/Container'
 import Row from '../components/utility/layout/Row'
 import Column from '../components/utility/layout/Column'
-import Icon from '../components/atoms/icon/Icon'
-import carIcon from '../src/img/icons/car.svg'
+import Panel from '../components/molecules/panel/Panel'
+import PanelHeader from '../components/atoms/panel/PanelHeader'
+import Header from '../components/atoms/header/Header'
+import PanelBody from '../components/atoms/panel/PanelBody'
+import PanelFooter from '../components/atoms/panel/PanelFooter'
+import Toggle from '../components/atoms/toggle/Toggle'
+import Button from '../components/atoms/button/Button'
 
 const Playground = () => {
-
-    const divStyle = {
-        backgroundColor: '#00448c',
-        width: '100%',
-        padding: '1em',
-        color: 'white',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center'
-    }
 
     return (
         <>
@@ -25,15 +19,44 @@ const Playground = () => {
                 <div className="playground-container__item">
                     <Container>
                         <Row>
-                            <Column xs={12}>
-                                <Icon svgImage={carIcon} svgWidth={15} svgHeight={15} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={20} svgHeight={20} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={25} svgHeight={25} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={30} svgHeight={30} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={15} svgHeight={15} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={20} svgHeight={20} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={25} svgHeight={25} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={30} svgHeight={30} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
+                            <Column xs={12} md={6} lg={4}>
+                                <Panel>
+                                    <PanelHeader>
+                                        <Header headerType="h2">This is a panel title</Header>
+                                    </PanelHeader>
+                                    <PanelBody>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    </PanelBody>
+                                    <PanelFooter centerContent={true}>
+                                        <Toggle nameLeft="stuff" nameRight="stufffff" />
+                                    </PanelFooter>
+                                </Panel>
+                            </Column>
+                            <Column xs={12} md={6} lg={4}>
+                                <Panel theme="dark" centerContent={true}>
+                                    <PanelHeader>
+                                        <Header headerType="h2">This is a panel title with centered content</Header>
+                                    </PanelHeader>
+                                    <PanelBody>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    </PanelBody>
+                                    <PanelFooter>
+                                        <Button theme="primary">A panel button</Button>
+                                    </PanelFooter>
+                                </Panel>
+                            </Column>
+                            <Column xs={12} md={6} lg={4}>
+                                <Panel theme="pattern" centerContent={true}>
+                                    <PanelHeader>
+                                        <Header headerType="h2">This is a panel title with centered content</Header>
+                                    </PanelHeader>
+                                    <PanelBody>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    </PanelBody>
+                                    <PanelFooter>
+                                        <Button theme="primary">A panel button</Button>
+                                    </PanelFooter>
+                                </Panel>
                             </Column>
                         </Row>
                     </Container>

78cd601cf677079b1a3053a1098ef2295ba03a33
Scott Quested <ScottQuested@hastingsdirect.com>
2021-03-17 11:32:44 +0000

    SQ Create new chat icon component. Add SCSS styles for component.

---
+++ components/atoms/chat/Chat.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import Icon from '../icon/Icon'
+import './chat.scss'
+import ChatIcon from '../../../src/img/icons/chat.svg'
+
+const Chat = props => {
+
+    const {
+        isSmall = false,
+        className = '',
+        children
+    } = props
+
+    const chatClass = classNames('a-chat', className, {
+        'a-chat--small': isSmall
+    })
+
+    return(
+        <div className={chatClass}>
+            <span className="a-chat__label">{children}</span> <Icon svgWidth={25} svgHeight={25} svgFill="#fff" svgImage={ChatIcon} />
+        </div>
+    )
+}
+
+Chat.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.string
+}
+
+export default Chat

6476f5acee4135b430660fc0436fcc3af074a3c9
Scott Quested <ScottQuested@hastingsdirect.com>
2021-03-17 17:17:10 +0000

    SQ Add new test for overlay component. Update Icon to use an onClick.

--- playground/index.js
+++ playground/index.js
@@ -5,6 +5,8 @@ import Row from '../components/utility/layout/Row'
 import Column from '../components/utility/layout/Column'
 import Icon from '../components/atoms/icon/Icon'
 import carIcon from '../src/img/icons/car.svg'
+import Overlay from '../components/molecules/overlay/Overlay'
+import Header from '../components/atoms/header/Header'
 
 const Playground = () => {
 
@@ -26,14 +28,18 @@ const Playground = () => {
                     <Container>
                         <Row>
                             <Column xs={12}>
-                                <Icon svgImage={carIcon} svgWidth={15} svgHeight={15} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={20} svgHeight={20} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={25} svgHeight={25} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={30} svgHeight={30} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={15} svgHeight={15} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={20} svgHeight={20} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={25} svgHeight={25} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={30} svgHeight={30} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
+                                <Overlay isOpen={true} >
+                                    <Header headerType="h1">How will the car be used?</Header>
+                                    <strong>Socially</strong>
+                                    <p>Visiting friends and family, shopping, going out, the school run, holidays in the UK - normal personal use.</p>
+                                    <strong>Socially and driving to and from a workplace</strong>
+                                    <p>All the above, plus travelling to and from one work address.</p>
+                                    <strong>Socially, driving to a workplace and for business use</strong>
+                                    <p>This is all of the above, plus any business travel to anywhere other than a main work address. This could be driving to visit customers or to attend conferences and is applicable to you and anyone else on the policy.</p>
+                                </Overlay>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             </Column>
                         </Row>
                     </Container>

5fb7a5bc32f1a0e7a08886a0e6d9d17b45f85c5d
Scott Quested <ScottQuested@hastingsdirect.com>
2021-03-17 10:00:15 +0000

    SQ Add test for new dropdown component.

--- playground/index.js
+++ playground/index.js
@@ -5,19 +5,18 @@ import Row from '../components/utility/layout/Row'
 import Column from '../components/utility/layout/Column'
 import Icon from '../components/atoms/icon/Icon'
 import carIcon from '../src/img/icons/car.svg'
+import Dropdown from '../components/atoms/dropdown/Dropdown'
 
 const Playground = () => {
 
-    const divStyle = {
-        backgroundColor: '#00448c',
-        width: '100%',
-        padding: '1em',
-        color: 'white',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center'
-    }
+    const options = [
+        'Car policy',
+        'Van policy',
+        'Bike policy',
+        'Home policy',
+        'Multicar policy',
+        'YouDrive policy',
+    ]
 
     return (
         <>
@@ -26,14 +25,16 @@ const Playground = () => {
                     <Container>
                         <Row>
                             <Column xs={12}>
-                                <Icon svgImage={carIcon} svgWidth={15} svgHeight={15} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={20} svgHeight={20} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={25} svgHeight={25} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={30} svgHeight={30} svgFill="white" svgBackgroundColor="#0085ff" isRound={true}/>
-                                <Icon svgImage={carIcon} svgWidth={15} svgHeight={15} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={20} svgHeight={20} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={25} svgHeight={25} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
-                                <Icon svgImage={carIcon} svgWidth={30} svgHeight={30} svgFill="white" svgBackgroundColor="#0085ff" isSquare={true}/>
+                                <Dropdown theme="dark" options={options} name="choosePolicy" label="Please choose your policy" />
+                            </Column>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="playground-container__item">
+                    <Container>
+                        <Row>
+                            <Column xs={12}>
+                                <Dropdown options={options} name="choosePolicy" label="Please choose your policy" />
                             </Column>
                         </Row>
                     </Container>

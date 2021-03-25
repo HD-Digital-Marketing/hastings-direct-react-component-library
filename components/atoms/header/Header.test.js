6fc6a71fa3110f87adad38a14627c80c5591a01d
Scott Quested <ScottQuested@hastingsdirect.com>
2021-03-25 17:16:35 +0000

    SQ Clean up test and re-run for components using header comp.

--- components/atoms/header/Header.test.js
+++ components/atoms/header/Header.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Header from './Header';
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Header from './Header'
 
 describe('Check the title renders', () => {
     test('it should match the snapshot', () => {
 
         const tree = renderer
             .create(<Header headerType="h3" theme="primary">I am a h3 title</Header>)
-            .toJSON();
-        expect(tree).toMatchSnapshot();
+            .toJSON()
+        expect(tree).toMatchSnapshot()
 
-    });
-});
+    })
+})
